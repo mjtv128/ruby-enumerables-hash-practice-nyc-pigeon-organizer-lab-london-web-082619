be6a100a61d463f43dffc82c24637774486587c8
jocular-source-7570 <mjtvan@gmail.com>
2019-08-17 00:40:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,13 +3,20 @@ def nyc_pigeon_organizer(data)
   data.each{|attribute, data_hash|
   data_hash.each{|property, names|
   names.each{|name|
-  new[name] = {:color => [], :gender: [], :live => []}
+   if !new.has_key?(name)
+     new[name] = {} 
+   end 
+   
+   if !new[name].has_key?(attribute)
+     new[name][attribute] = []
+   end 
+   
+   if !new[name][attribute].include?(property)
+     new[name][attribute] << property.to_s 
+   end 
   }
   }
-  }
+  } 
+ new
   
-  data[:color].each{|property, names|
-  names.each{|name|
-  if data[:color][property].include?(name)
-    new[name][:color] << attribute.to_s 
 end
