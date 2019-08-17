f493eff0bc16651796f01d14dcb4da540af8a1a8
jocular-source-7570 <mjtvan@gmail.com>
2019-08-17 00:40:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new = {}
+  data.each{|attribute, data_hash|
+  data_hash.each{|property, names|
+  names.each{|name|
+   if !new.has_key?(name)
+     new[name] = {} 
+   end 
+   
+   if !new[name].has_key?(attribute)
+     new[name][attribute] = []
+   end 
+   
+   if !new[name][attribute].include?(property)
+     new[name][attribute] << property.to_s 
+   end 
+  }
+  }
+  } 
+ new
+  
 end
